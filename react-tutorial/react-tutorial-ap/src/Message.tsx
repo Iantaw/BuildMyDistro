7c9deb168d3d33577f8f3ae1db64a33985f9563e
Iantaw <ian@tawileh.net>
2025-06-23 21:03:07 -0400

    Begun React Tutorial and created Hello World Page

---
+++ react-tutorial/react-tutorial-ap/src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing (Capatalize first letter of each word)
+function Message() {
+    // JSX: Javascript XML
+    const name = 'Ian';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
